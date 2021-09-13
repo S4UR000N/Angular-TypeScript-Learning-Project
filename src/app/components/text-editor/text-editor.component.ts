724f75087a08da601bc95dea785484c034496b1e
rascic <rascic@cmbetsolutions.com>
2021-09-13 15:07:05 +0200

    NgForOf (*ngFor) Directive implementation with list of a random length

--- src/app/components/text-editor/text-editor.component.ts
+++ src/app/components/text-editor/text-editor.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-editor.component.css']
 })
 export class TextEditorComponent implements OnInit {
-  hadInteractionInput: boolean = false;
   colors = {
     DEFAULT: "default",
     RED: "red",
@@ -23,7 +22,28 @@ export class TextEditorComponent implements OnInit {
 
   output: string = "Example Input";
 
-  constructor() {}
+  hadInteractionInput: boolean = false;
+
+  luckyCrypto: string[] = [];
+
+  constructor() {
+    // random string generator
+    let pass = (n: number) => [...crypto.getRandomValues(new Uint8Array(n))]
+    .map((x,i)=>(i=x/255*61|0,String.fromCharCode(i+(i>9?i>35?61:55:48)))).join('');
+
+    // random number generator
+    let getRandomNumberBetween = (min: number, max: number) => {
+      return Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    // random length applied to random string using random number
+    let myLen = getRandomNumberBetween(1, 10);
+    for(let i = 0; i < myLen; i++) {
+      let r = pass(getRandomNumberBetween(1, 1000));
+      // @ts-ignore: this.prop is really assigned before being used
+      this.luckyCrypto.push(r);
+    }
+  }
 
   ngOnInit(): void {}
 }

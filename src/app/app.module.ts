18d75b5ee69ddda171cdb3bdd0c83bbf6c1d6a55
rascic <rascic@cmbetsolutions.com>
2021-09-13 10:39:09 +0200

    Project setup with basic UI (BS 5) and text writing & styling functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+
 
 import { AppComponent } from './app.component';
+import { TextEditorComponent } from './components/text-editor/text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/text-editor/text-editor.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-editor',
+  templateUrl: './text-editor.component.html',
+  styleUrls: ['./text-editor.component.css']
+})
+export class TextEditorComponent implements OnInit {
+  colors = {
+    DEFAULT: "default",
+    RED: "red",
+    BLUE: "blue"
+  }
+  decorations = {
+    NONE: "none",
+    TEXT_DECORATION_UNDERLINE: "text-decoration-underline",
+    TEXT_DECORATION_LINE_THROUGH: "text-decoration-line-through"
+  }
+
+  color: string = 'default';
+  decoration: string = "default";
+  output: string = "Example Input";
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
